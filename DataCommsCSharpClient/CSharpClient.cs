2ad21269fd03ecdebfde48fbf551bbef917ed09b
Michael Ranciglio <rancigliom@gmail.com>
2017-12-03 20:26:08 -0600

    Initial Commit

---
+++ DataCommsCSharpClient/CSharpClient.cs
@@ -0,0 +1,24 @@
+﻿/* CSharpClient.cs
+ * 
+ * (C) 2017 Michael Ranciglio
+ * 
+ * Prepared for CS480 at Southeast Missouri State University
+ * 
+ * Modeled after the DataCommsCppServer.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCommsCSharpClient
+{
+	class CSharpClient
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
